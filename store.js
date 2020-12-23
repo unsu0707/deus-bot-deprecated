5bf46754100e5a307ab61bfd9a681f793cde1080
unsu0707 <eunsu.jang@rakuten.com>
2020-12-23 10:49:13 +0900

    move db.json to .data/db.json

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const global = require('./global');
-const adapter = new FileSync('db.json');
+const adapter = new FileSync('.data/db.json');
 const db = low(adapter);
 
 
